cc41a635c977699078c2f3dab22d09864381c298
jxlaurich <xlj1025@gmail.com>
2024-03-15 17:11:33 -0500

    Quartz sync: Mar 15, 2024, 5:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Uncial Antiqua",
+        header: "Inknut Antiqua",
         body: "Inknut Antiqua",
         code: "IBM Plex Mono",
       },
